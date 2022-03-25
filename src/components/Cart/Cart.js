6b400967e9090a8fb2460840d8c12f7bbaf80f4f
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-03-26 05:06:55 +0600

    cart added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css';
+const Cart = ({cart}) => {
+    return (
+        <div>
+            <h4>Order Summary</h4>
+                <p>Seleted Items :{cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
