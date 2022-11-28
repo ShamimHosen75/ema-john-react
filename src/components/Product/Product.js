08d2187c4c93690c36cbf0dcc2b9089a875d9ac7
ShamimHosen75 <developer.shamimhosen@gmail.com>
2022-11-29 03:24:10 +0600

    some chnages

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -11,13 +11,12 @@ const Product = (props) => {
             <img src={img} alt="" />
             <div className='product-info'>
                     <p className='product-name'>{name}</p>
-                    <p>Price :${price}</p>
-                    <p><small>Seller :{seller}</small></p>
-                    <p><small>Ratings :{ratings} stars</small></p>
+                    <p>Price : ${price}</p>
+                    <p><small>Seller : {seller}</small></p>
+                    <p><small>Ratings : {ratings} stars</small></p>
             </div>
             <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
                 <p>Add to Cart</p>
-                
             </button>
         </div>
     );
